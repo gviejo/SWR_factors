646fbe16e4062ecb365aa5f7ad2e8ddb95304d5a
gviejo <guillaume.viejo@gmail.com>
2019-04-26 11:02:19 -0400

    testing with less factors

--- python/main_load_sav.py
+++ python/main_load_sav.py
@@ -22,23 +22,30 @@ files = os.listdir(data_directory)
 samples = {}
 
 for f in files:
-	k = f.split("_")[0]
-	if k not in samples:
-		samples[k] = {'train':{}, 'valid':{}}
-	if 'train' in f:
-		samples[k]['train'] = read_data(os.path.join(data_directory, f))
-	elif 'valid' in f:
-		samples[k]['valid'] = read_data(os.path.join(data_directory, f))
+	k = f.split("_")[0]	
+	samples[k] = read_data(os.path.join(data_directory, f))
 
+
+datasets = pickle.load(open("../data/swr_hist_Mouse12.pickle", "rb"))
+datasets = {k.split("/")[1]:datasets[k] for k in datasets.keys()}
+
+
+def ploti(s,k=0):
+	n = datasets[s]['train_data'].shape[-1]
+	for i in range(n):
+		plot(datasets[s]['train_data'][k,:,i]+i*2, color = 'black')
+		plot(samples[s]['output_dist_params'][k,:,i]+i*2, color = 'red')
+		axvline(100, alpha = 0.5)
 
 sys.exit()
+
 
 
 factors = []
 
 for s in samples.keys():
-	dims = samples[s]['train']['factors'].shape
-	factors.append(samples[s]['train']['factors'].reshape(dims[0], dims[1] * dims[2]))
+	dims = samples[s]['factors'].shape
+	factors.append(samples[s]['factors'].reshape(dims[0], dims[1] * dims[2]))
 	# factors.append(samples[s]['train']['factors'][:,0,:])
 
 factors = np.vstack(factors)
@@ -47,6 +54,11 @@ from sklearn.manifold import TSNE
 
 X = TSNE(n_components=2, perplexity = 30).fit_transform(factors)
 
+
+from pylab import *
+
 scatter(X[:,0], X[:,1])
+
+show()
 
 
